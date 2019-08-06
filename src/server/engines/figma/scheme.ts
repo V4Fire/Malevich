4b72d1108f9eade7e4b82ee194c98ceea15908d1
dmitrybrovka <dmitrybrovka@yandex.ru>
2019-08-07 00:29:05 +0300

    :wrench: Improvements

--- src/server/engines/figma/scheme.ts
+++ src/server/engines/figma/scheme.ts
@@ -61,31 +61,9 @@ export default {
 	color: f(storeColor)
 };
 
-const adapters = {
+const adapters: Record<string, Record<string, Function>> = {
 	bButton: {
-		size: (el: Figma.Node) => {
-			const
-				background = $C(el.children).one.get((c) => c.name === 'background'),
-				text = $C(el.children).one.get((c) => c.name.toLowerCase() === 'text');
-
-			if (!background || !text) {
-				WARNINGS.push({
-					name: `No text or background for ${el.name}`
-				});
-			}
-
-			const
-				b = background.absoluteBoundingBox,
-				t = text.absoluteBoundingBox;
-
-			return {
-				horOffset: Math.abs(b.y - t.y),
-				vertOffset: Math.abs(b.x - t.x),
-				textHeight: t.height,
-				height: b.height,
-				fontSize: text.styles.fontSize
-			};
-		},
+		size: simpleSize,
 
 		hover: buttonState,
 		active: buttonState,
@@ -110,13 +88,159 @@ const adapters = {
 				backgroundColor: bg || 'none'
 			};
 		},
+		preIcon: buttonWithIcon,
+		postIcon: (el: Figma.Node) => buttonWithIcon(el, 'post')
+	},
 
-		preIcon: withIcon,
-		postIcon: (el: Figma.Node) => withIcon(el, 'post')
+	bInput: {
+		size(el: Figma.Node): Dictionary {
+			const
+				placeholder = $C(el.children).one.get((c) => c.name === 'placeholder'),
+				pFill = $C(placeholder).get('fills.0');
+
+			return {
+				placeholderColor: pFill && calcColor(pFill),
+				placeholderStyle: placeholder && $C(RAW.styles).get(`${placeholder.styles.text}.name`),
+				...simpleSize(el)
+			};
+		},
+
+		preIcon: inputWithIcon,
+		postIcon: inputWithIcon,
+		readonly(el: Figma.Node): Dictionary {
+			const
+				value = $C(el.children).one.get((c) => c.name === 'value'),
+				base = $C(el.children).one.get((c) => c.name === 'background');
+
+			if (!value || !base) {
+				WARNINGS.push({
+					name: `No background or value for ${el.name}`
+				});
+			}
+
+			let
+				textColor = $C(value).get('fills.0'),
+				baseBgColor = $C(base).get('children.0.fills.0'),
+				baseBorderColor = $C(base).get('children.0.strokes.0');
+
+			if (textColor) {
+				textColor = calcColor(textColor);
+			}
+
+			if (baseBgColor) {
+				baseBgColor = calcColor(baseBgColor);
+			}
+
+			if (baseBorderColor) {
+				baseBorderColor = calcColor(baseBorderColor);
+			}
+
+			return {
+				textColor,
+				baseBgColor,
+				baseBorderColor
+			};
+		},
+		focus: () => ({foo: 1}),
+		valid(el: Figma.Node): Dictionary {
+			const result = {
+				valid: {},
+				invalid: {}
+			};
+
+			const
+				callAdaptersForMod = (mod, m) => {
+					const
+						adapter = adapters.bInput[mod];
+
+					if (Object.isFunction(adapter)) {
+						return adapter(m);
+					}
+				};
+
+			const
+				state = {true: 'valid', false: 'invalid'};
+
+			$C(el.children).forEach((valueGroup) => {
+				// true false group if layers
+				$C(valueGroup.children).forEach((layer) => {
+					if (/m:\w+/.test(layer.name)) {
+						const
+							name = layer.name.replace('m:', '');
+
+						return result[state[valueGroup.name]][name] = callAdaptersForMod(name, layer);
+					}
+				});
+			});
+
+			return result;
+		}
 	}
 };
 
-function withIcon(el: Figma.Node, pos: 'pre' | 'post' = 'pre'): Dictionary {
+function inputWithIcon(el: Figma.Node): Dictionary {
+	const
+		icon = $C(el.children).one.get((c) => c.name === 'icon'),
+		iconBackground = $C(el.children).one.get((c) => c.name === 'iconBackground');
+
+	if (!iconBackground || !icon) {
+		WARNINGS.push({
+			name: `No icon or background for ${el.name}`
+		});
+	}
+
+	const
+		{absoluteBoundingBox: i} = icon,
+		{absoluteBoundingBox: b} = iconBackground;
+
+	const
+		bgLayer = $C(iconBackground).get('children.0'),
+		fill = $C(bgLayer).get('fills.0'),
+		stroke = $C(bgLayer).get('strokes.0'),
+		strokeWeight = $C(bgLayer).get('strokeWeight');
+
+	return {
+		iconSize: i.width,
+		offset: Math.abs(i.x - b.x),
+
+		baseBgColor: calcColor(fill),
+		baseStrokeWeight: strokeWeight,
+		baseStrokeColor: calcColor(stroke)
+	};
+}
+
+function simpleSize(el: Figma.Node): Dictionary {
+	const
+		background = $C(el.children).one.get((c) => c.name === 'background'),
+		text = $C(el.children).one.get((c) => c.name.toLowerCase() === 'text' || c.name.toLowerCase() === 'value');
+
+	if (!background || !text) {
+		WARNINGS.push({
+			name: `No text or background for ${el.name}`
+		});
+	}
+
+	const
+		b = background.absoluteBoundingBox,
+		t = text.absoluteBoundingBox,
+		fontOptions = RAW.styles[text.styles.text];
+
+	let textStyle;
+
+	if (fontOptions) {
+		textStyle = (<Figma.Style>fontOptions).name;
+	}
+
+	return {
+		horOffset: Math.abs(b.y - t.y),
+		vertOffset: Math.abs(b.x - t.x),
+		textHeight: t.height,
+		height: b.height,
+		textStyle
+	};
+}
+
+function buttonWithIcon(el: Figma.Node, pos: 'pre' | 'post' = 'pre'): Dictionary {
 	const
 		icon = $C(el.children).one.get((c) => c.name === 'icon'),
 		text = $C(el.children).one.get((c) => c.name === 'text');
@@ -183,7 +307,7 @@ export function writeComponent(name: string, el: Figma.Node): void {
 					adapter = $C(adapters).get(`${name}.${mod}`);
 
 				if (mod && Object.isFunction(adapter)) {
-					link.mods[mod] = adapters[name][mod](c);
+					link.mods[mod] = adapter(c);
 				}
 			}
 		});
@@ -225,7 +349,6 @@ export function writeComponent(name: string, el: Figma.Node): void {
 				adapter = $C(adapters).get(`${name}.exterior`);
 
 			if (Object.isFunction(adapter)) {
-
 				if (!link.exterior) {
 					link.exterior = {};
 				}
